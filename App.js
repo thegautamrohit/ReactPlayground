faaabb8f35d409bda9edf610356c560fb8cf085a
thegautamrohit <rgautam1997@gmail.com>
2023-08-13 00:19:44 +0530

    node modules added to git ignore

--- App.js
+++ App.js
@@ -1,1 +1,9 @@
-let a = 1;
+const heading1 = React.createElement("h1", { id: "h1" }, "Hello H1");
+const heading2 = React.createElement("h2", { id: "h2" }, "Hello H2");
+
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]); // we are going to use an array
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
